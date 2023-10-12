803347371655ecf06c8b845e88eb2b7897f90085
DDUDII <131181283+DDUDII@users.noreply.github.com>
2023-10-12 19:11:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.16 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    if(arr.length <= 1){
+        return [-1];
+    }
+ const min = Math.min(...arr);
+ const result = arr.filter((num) => num !== min); 
+  return result;
+
+}
